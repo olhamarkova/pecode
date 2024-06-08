3f0eb1876c3e68e6dafa1d24cfad096d02ca96f6
olhamarkova <olga.mulchenko@gmail.com>
2024-06-08 13:54:07 +0300

    set up mochawesome reporter

--- cypress.config.js
+++ cypress.config.js
@@ -2,15 +2,24 @@ const { defineConfig } = require("cypress");
 require("dotenv").config();
 
 module.exports = defineConfig({
+  reporter: "cypress-mochawesome-reporter",
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: "test-assignment",
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
   env: {
     url: `${process.env.URL}`,
   },
   viewportWidth: 1920,
   viewportHeight: 1080,
-  video: true,
+  //video: true,
+  screenshotOnRunFailure: true,
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require("cypress-mochawesome-reporter/plugin")(on);
     },
     specPattern: "cypress/e2e/*/*.cy.js",
   },
